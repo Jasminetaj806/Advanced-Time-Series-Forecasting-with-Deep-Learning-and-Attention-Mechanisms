d47801c57ce864ecb3c8443741dd9f5e4e6a1314
Jasminetaj806 <jasminetajjms@gmail.com>
2026-02-07 05:13:31 +0530

    Delete cofig.py

--- cofig.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-INPUT_DIM = 5        # total features including target
-TARGET_DIM = 1
-ENC_HIDDEN = 64
-DEC_HIDDEN = 64
-HORIZON = 10
