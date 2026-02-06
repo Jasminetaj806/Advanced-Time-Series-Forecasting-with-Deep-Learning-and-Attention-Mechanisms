3b566ead718d692d3fcff98981fcd7ea449b35fb
Jasminetaj806 <jasminetajjms@gmail.com>
2026-02-07 05:10:18 +0530

    Delete baselinemodel.py

--- baselinemodel.py
+++
@@ -1,9 +0,0 @@
-class BaselineLSTM(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lstm = nn.LSTM(4, 64, batch_first=True)
-        self.fc = nn.Linear(64, HORIZON)
-
-    def forward(self, x):
-        _, (h, _) = self.lstm(x)
-        return self.fc(h[-1])
